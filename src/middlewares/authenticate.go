5d443b2e58e761f460e75e9d5470fce2ba026c58
danilocarsan <danilocarsan@gmail.com>
2022-08-24 12:20:34 -0300

    adicionando verificação de autorização

--- src/middlewares/authenticate.go
+++ src/middlewares/authenticate.go
@@ -1,12 +1,21 @@
 package middlewares
 
 import (
+	"gotaskapp/src/security"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 // Verify user authenticated
 func Authenticate() gin.HandlerFunc {
-	return func(ctx *gin.Context) {
+	return func(c *gin.Context) {
 
+		if err := security.ValidateJwtToken(c.Request); err != nil {
+			c.JSON(http.StatusUnauthorized, err.Error())
+			return
+		}
+
+		c.Next()
 	}
 }
